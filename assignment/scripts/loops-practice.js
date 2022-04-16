b0425d71e1e2915231315337d37c019890103c74
Juan Villegas <jvillegas422@gmail.com>
2022-04-16 03:15:45 -0500

    Question 1c updated

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,7 +14,7 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 // The stopping condition is changed from 4 to 6, which will count from 0 to 5
-for (let i=0; i<6; i++) {  
+for (let i = 0; i < 6; i++) {  
   console.log(i);
 }
 
@@ -22,13 +22,20 @@ for (let i=0; i<6; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 // The initialization i=0 is changed to i=3; which will count from 3 to 5
-for (let i=3; i < 6; i++) {
+for (let i = 3; i < 6; i++) {
   console.log(i);
 }
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+// The initialization starts with i=1; and stopping condition is set to i<11; and adding 1 to i
+// Then create an if using modulus (division remainder) with remainder of 0, output will only provide even numbers
+for (let i = 1; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i);  
+  }
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
